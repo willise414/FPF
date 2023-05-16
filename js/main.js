d90e6266af3b730849fb7ec609801cf5f7c13913
Sean Williams <willise414@gmail.com>
2023-05-16 20:27:52 -0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+openNavBtn.addEventListener("click", openNav);
+closeNavBtn.addEventListener("click", closeNav);
+
+window.onresize = function () {
+  location.reload();
+};
